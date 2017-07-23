2af9d58a1d3a3f0dd07ce44d1a3e72291761cafe
EddieZC <eddi_tlv_1007@hotmail.com>
2017-07-22 21:35:03 -0500

    probando4

--- app/src/main/java/pe/gob/inei/encuestahabilidades/fragments/Modulo8Fragment3.java
+++ app/src/main/java/pe/gob/inei/encuestahabilidades/fragments/Modulo8Fragment3.java
@@ -18,7 +18,7 @@ public class Modulo8Fragment3 extends Fragment {
     public Modulo8Fragment3() {
         // Required empty public constructor
         //prueba2 :V
-        //prueba4
+        //prueba46
     }
 
 
